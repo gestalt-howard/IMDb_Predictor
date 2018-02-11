f5850300bfa1370ed25f86d4ff8c43a54c8a8dd2
Howard Tai <gestalt.howard@gmail.com>
2018-02-10 19:16:32 -0800

    progress push 2/10

--- imdb_neural_model.py
+++ imdb_neural_model.py
@@ -10,11 +10,11 @@ from keras.layers import Input, Dense, Activation, core, Dropout
 from keras.optimizers import Adam, RMSprop, SGD
 
 
-# IMDb
+# IMDb Training Model
 def imdb_regression(size):
     # Input vector length on order of 22,000 elements
-    inputs = Input(size)
-    dense1 = Dense(32768, name='dense1')(inputs)
+    inputs = Input(size)  # Needs tuple input
+    dense1 = Dense(16192, name='dense1')(inputs)
     dense2 = Dropout(0.2)(dense1)  # Drop percentage of values to prevent over-fitting
     # Can include as many layers in between (Dense and Dropout)
     dense3 = Dense(8096, name='dense3')(dense2)
@@ -35,9 +35,9 @@ def imdb_regression(size):
 
 # Non-linear learning equivalent of PCA
 def auto_encoder(size):
-    inputs = Input(size)
+    inputs = Input(size)  # Needs tuple input
     # Reduce dimensionality
-    encoder = Dense(19000, activation='relu', name='encoder')(inputs)
+    encoder = Dense(10000, activation='relu', name='encoder')(inputs)
     # Bring it back in dimensionality
     autoencoder = Dense(size[0], activation='sigmoid', name='autoencoder')(encoder)
 
